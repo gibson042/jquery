7a2d525c43c6a02a00d99d440760063e4cc36b4c
Richard Gibson <richard.gibson@gmail.com>
2013-10-31 09:30:45 -0400

    save 8 bytes

--- src/toDecimal.js
+++ src/toDecimal.js
@@ -1,11 +1,11 @@
 define(function() {
 
-function toDecimal( num, sign, point, whole, fraction, e, exponent ) {
+function toDecimal( num, sign, whole, point, fraction, e, exponent ) {
 	return !e ?
 		num :
 		exponent > 0 ?
-			sign + (whole || "") + fraction + Array( exponent - (point && fraction.length) + 1 ).join( 0 ) :
-			sign + "." + Array( -exponent ).join( 0 ) + (whole || "") + fraction;
+			sign + whole + fraction + Array( exponent - (point && fraction.length) + 1 ).join( 0 ) :
+			sign + "." + Array( -exponent ).join( 0 ) + whole + fraction;
 }
 
 return toDecimal;
