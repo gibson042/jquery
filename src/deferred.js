38eeb0f3b91c47e1a71925d3615c815a586c0f8e
Richard Gibson <richard.gibson@gmail.com>
2014-11-03 11:58:56 -0500

    Deferred: Pass Promises/A+ suite

--- src/deferred.js
+++ src/deferred.js
@@ -4,13 +4,22 @@ define([
 	"./callbacks"
 ], function( jQuery, slice ) {
 
+function Identity( v ) {
+	return v;
+}
+function Thrower( ex ) {
+	throw ex;
+}
+
 jQuery.extend({
 
 	Deferred: function( func ) {
 		var tuples = [
-				// action, add listener, listener list, final state
-				[ "resolve", "done", jQuery.Callbacks("once memory"), "resolved" ],
-				[ "reject", "fail", jQuery.Callbacks("once memory"), "rejected" ],
+				// action, add listener, callback list, .then list, final state
+				[ "resolve", "done", jQuery.Callbacks("once memory"),
+					jQuery.Callbacks("once memory"), "resolved" ],
+				[ "reject", "fail", jQuery.Callbacks("once memory"),
+					jQuery.Callbacks("once memory"), "rejected" ],
 				[ "notify", "progress", jQuery.Callbacks("memory") ]
 			],
 			state = "pending",
@@ -22,29 +31,106 @@ jQuery.extend({
 					deferred.done( arguments ).fail( arguments );
 					return this;
 				},
-				then: function( /* fnDone, fnFail, fnProgress */ ) {
-					var fns = arguments;
-					return jQuery.Deferred(function( newDefer ) {
-						jQuery.each( tuples, function( i, tuple ) {
-							var fn = jQuery.isFunction( fns[ i ] ) && fns[ i ];
-							// deferred[ done | fail | progress ] for forwarding actions to newDefer
-							deferred[ tuple[1] ](function() {
-								var returned = fn && fn.apply( this, arguments );
-								if ( returned && jQuery.isFunction( returned.promise ) ) {
-									returned.promise()
-										.done( newDefer.resolve )
-										.fail( newDefer.reject )
-										.progress( newDefer.notify );
-								} else {
-									newDefer[ tuple[ 0 ] + "With" ](
-										this === promise ? newDefer.promise() : this,
-										fn ? [ returned ] : arguments
-									);
+				then: function( fnDone, fnFail, fnProgress ) {
+					var resolved;
+
+					function thenCallback( deferred, callback, bound ) {
+						return function( value ) {
+							var fn = function() {
+								var returned, then;
+
+								if ( resolved !== bound ) {
+									return;
 								}
-							});
+
+								try {
+									returned = callback( value );
+									then = returned &&
+										// Support: Promises/A+ 2.3.4
+										// 2. Requirements
+										//    3. The Promise Resolution Procedure
+										//       (`[[Resolve]](promise, x)`)
+										//       4. If `x` is not an object or function, fulfill
+										//          `promise` with `x`.
+										( typeof returned === "object" ||
+											typeof returned === "function" ) &&
+										returned.then;
+
+									// Support: Promises/A+ 2.3.1
+									// 2. Requirements
+									//    3. The Promise Resolution Procedure
+									//       (`[[Resolve]](promise, x)`)
+									//       1. If `promise` and `x` refer to the same object,
+									//          reject `promise` with a `TypeError` as the reason.
+									if ( returned === deferred.promise() ) {
+										throw new TypeError();
+									}
+
+									if ( jQuery.isFunction( then ) ) {
+										resolved = returned;
+										then.call(
+											returned,
+											thenCallback( deferred, Identity, returned ),
+											thenCallback( deferred, Thrower, returned )
+										);
+									} else {
+										deferred.resolve( returned );
+									}
+								} catch ( e ) {
+
+									// Support: Promises/A+ 2.3.3.iii.d.a
+									// 2. Requirements
+									//    3. The Promise Resolution Procedure
+									//       (`[[Resolve]](promise, x)`)
+									//       3. Otherwise, if `x` is an object or function,
+									//          iii. If `then` is a function, call it with `x` as
+									//               `this`, first argument `resolvePromise`, and
+									//               second argument `rejectPromise`, where:
+									//               d. If calling `then` throws an exception `e`,
+									//                  a. If `resolvePromise` or `rejectPromise`
+									//                     have been called, ignore it.
+									if ( resolved === bound || resolved === returned ) {
+										deferred.reject( e );
+									}
+								}
+							};
+							if ( bound ) {
+								fn();
+							} else {
+								setTimeout(fn);
+							}
+						};
+					}
+
+					return jQuery.Deferred(function( newDefer ) {
+						tuples[ 0 ][ 3 ].add(
+							thenCallback( newDefer, jQuery.isFunction( fnDone ) ?
+								fnDone :
+								Identity
+							)
+						);
+						tuples[ 1 ][ 3 ].add(
+							thenCallback( newDefer, jQuery.isFunction( fnFail ) ?
+								fnFail :
+								Thrower
+							)
+						);
+						deferred.progress(function() {
+							var fn = jQuery.isFunction( fnProgress ) && fnProgress,
+								returned = fn && fn.apply( this, arguments );
+							if ( returned && jQuery.isFunction( returned.promise ) ) {
+								returned.promise().progress( newDefer.notify );
+							} else {
+								newDefer.notifyWith(
+									this === promise ? newDefer.promise() : this,
+									fn ? [ returned ] : arguments
+								);
+							}
 						});
-						fns = null;
 					}).promise();
+				},
+				catch: function( fnFail ) {
+					return promise.then( null, fnFail );
 				},
 				// Get a promise for this deferred
 				// If obj is provided, the promise aspect is added to the object
@@ -54,25 +140,28 @@ jQuery.extend({
 			},
 			deferred = {};
 
-		// Keep pipe for back-compat
-		promise.pipe = promise.then;
-
 		// Add list-specific methods
 		jQuery.each( tuples, function( i, tuple ) {
 			var list = tuple[ 2 ],
-				stateString = tuple[ 3 ];
+				stateString = tuple[ 4 ];
 
 			// promise[ done | fail | progress ] = list.add
 			promise[ tuple[1] ] = list.add;
 
 			// Handle state
 			if ( stateString ) {
-				list.add(function() {
-					// state = [ resolved | rejected ]
-					state = stateString;
+				list.add(
+					function() {
+						// state = [ resolved | rejected ]
+						state = stateString;
+					},
 
-				// [ reject_list | resolve_list ].disable; progress_list.lock
-				}, tuples[ i ^ 1 ][ 2 ].disable, tuples[ 2 ][ 2 ].lock );
+					// [ reject_list | resolve_list ].disable; progress_list.lock
+					tuples[ i ^ 1 ][ 2 ].disable, tuples[ 2 ][ 2 ].lock,
+
+					// fire .then callbacks first
+					tuple[ 3 ].fire
+				);
 			}
 
 			// deferred[ resolve | reject | notify ]
